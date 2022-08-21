e845ebc4dfdbbb808e3417b5228a5f33bfb08713
edith2302 <edith.parra1601@alumnos.ubiobio.cl>
2022-08-21 14:25:36 -0400

    Formato tabla PDF

--- frontend/views/usuario/_indexpdf1.php
+++ frontend/views/usuario/_indexpdf1.php
@@ -16,10 +16,54 @@ use yii\grid\GridView;
             ['class' => 'yii\grid\SerialColumn'],
 
             //'id_usuario',
-            'nombre',
-            'rut',
-            'email',
-            'telefono',
+            //'nombre',
+            //'rut',
+
+            [
+                'label'=>'Nombre',
+                'value'=>function ($model) { return $model['nombre'].' '.$model['apellido']; },
+                'format'=>'raw',
+                //'headerOptions' => ['width' => '1000px;','style'=>'text-align: center !important;'],
+                //'contentOptions' => ['style'=>'padding:10px 5px 0px 5px;text-align: center;'],
+            ],
+            [
+                'label'=>'Rut',
+                'value'=>function ($model) { 
+ 
+                    $formatRut = $model['rut'];
+ 
+                    if (strpos($formatRut, '-') !== false ) {
+             
+                        $splittedRut = explode('-', $formatRut);
+                        $number = number_format($splittedRut[0], 0, ',', '.');
+                        $verifier = strtoupper($splittedRut[1]);
+                        return $number . '-' . $verifier;
+                    }
+                    return number_format($formatRut, 0, ',', '.');
+                 
+                },
+                'format'=>'raw',
+                'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: center;'],
+            ],
+
+            [
+                'label'=>'E-mail',
+                'value'=>function ($model) { return $model['email']; },
+                'format'=>'raw',
+                'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                //'contentOptions' => ['style'=>'padding:10px 5px 0px 5px;text-align: center;'],
+            ],
+
+            [
+                'label'=>'Teléfono',
+                'value'=>function ($model) { return $model['telefono']; },
+                'format'=>'raw',
+                'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: center;'],
+            ],
+            //'email',
+            //'telefono',
             //'telefono_alternativo',
             
 
--- frontend/views/usuario/_indexpdf2.php
+++ frontend/views/usuario/_indexpdf2.php
@@ -15,15 +15,56 @@ use yii\grid\GridView;
             ['class' => 'yii\grid\SerialColumn'],
 
             //'id_usuario',
-            'nombre',
-            'rut',
-            'email',
-            'telefono',
+            //'nombre',
+            // 'rut',
+            //'email',
+            //'telefono',
             //'telefono_alternativo',
-            
-
-            
+            [
+                'label'=>'Nombre',
+                'value'=>function ($model) { return $model['nombre'].' '.$model['apellido']; },
+                'format'=>'raw',
+                //'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: left;'],
+            ],
            
+            [
+                'label'=>'Rut',
+                'value'=>function ($model) { 
+
+                    $formatRut = $model['rut'];
+
+                    if (strpos($formatRut, '-') !== false ) {
+            
+                        $splittedRut = explode('-', $formatRut);
+                        $number = number_format($splittedRut[0], 0, ',', '.');
+                        $verifier = strtoupper($splittedRut[1]);
+                        return $number . '-' . $verifier;
+                    }
+                    return number_format($formatRut, 0, ',', '.');
+                
+                },
+                'format'=>'raw',
+                'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: center;'],
+            ],
+
+            [
+                'label'=>'E-mail',
+                'value'=>function ($model) { return $model['email']; },
+                'format'=>'raw',
+                //'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: left;'],
+            ],
+
+            [
+                'label'=>'Teléfono',
+                'value'=>function ($model) { return $model['telefono']; },
+                'format'=>'raw',
+                //'headerOptions' => ['width' => '300px;','style'=>'text-align: center !important;'],
+                'contentOptions' => ['style'=>'padding:10px 10px 0px 10px;text-align: center;'],
+            ],
+  
         ],
     ]); ?>
 
